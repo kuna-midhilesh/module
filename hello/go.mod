ba353eb6579de547352b1771e654d89b47cd468b
kuna midhilesh <kuna.midhilesh@pepperstone.com>
2022-12-04 13:31:07 +0530

    second

--- hello/go.mod
+++ hello/go.mod
@@ -2,5 +2,6 @@ module hello
 
 go 1.18
 
+replace github.com/kuna-midhilesh/module/greetings => ../greetings
 
-replace greetings => ../greetings
+require github.com/kuna-midhilesh/module/greetings v0.0.0-00010101000000-000000000000
--- hello/main.go
+++ hello/main.go
@@ -1,9 +1,12 @@
-package main
-import(
-	"fmt"
-	"greetings"
-)
-func main(){
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+package hello
+
+import _ "github.com/kuna-midhilesh/module/greetings"
+type payroll struct{
+	employee greetings.
+
+
 }
+//func main(){
+//	message := greetings.Hello("Gladys")
+//	fmt.Println(message)
+//}
